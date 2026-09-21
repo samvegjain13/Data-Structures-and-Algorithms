bd742e38019192528fbd338276ee1d342d936374
Samveg Jain <samvegjain014@gmail.com>
2026-09-22 04:21:06 +0530

    Time: 3 ms (9.32%), Space: 10.1 MB (6.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <int> st;
+        int i,n;
+        n = s.size();
+        for(i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[') st.push(s[i]);
+            else {
+                if(st.empty())return false;
+                char ch = st.top();
+                st.pop();
+                if((s[i]=='}' && ch=='{') ||
+                   (s[i]==']' && ch=='[') ||
+                   (s[i]==')' && ch=='('));
+                else return false;
+            } 
+        } return st.empty();
+    }
+};
